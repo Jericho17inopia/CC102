90288f7541b3f37acac9e64d828ad33cf2b99388
Jericho17inopia <jerichoinopia7@gmail.com>
2024-10-03 11:36:20 +0800

    Add files via upload

---
+++ 02BasicOutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+
+using namespace std;
+
+int main ()
+{
+	cout << "Our Lady of Lourdes College Foundation";
+	cout << "College of Computer studies";
+	cout << " BSIT-1B";
+	cout << "Jericho inopia, Laureta";
+	cout << "Purok 2B, Brgy. Tanauan Capalonga Camarines Norte";
+	return 0;
+}
