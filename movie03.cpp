e05e0b513faa597e4cdd8b01dce147054ce603fa
Jericho Inopia <jerichoinopia7@gmail.com>
2024-11-28 11:42:35 +0800

    Add files via upload

---
+++ movie03.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int age;
+	double money;
+	bool parent;
+	string show, budget;
+	cout << "Enter your age: ";
+	cin >> age;
+	if (age<13) {
+		cout << "with parent?";
+		cin >> parent;
+		if (parent) 
+			show="PG & G show.";
+			show="G show.";
+	}
+	else if (age<16) { 
+		cout << "with Parent?";
+		cin >> parent;
+		if (parent)
+			show="R, PG & G show";
+		else 
+			show = "PG & G show";
+	}
+	else
+		show="R, PG & G show.";
+	cout << "Enter money:";
+	cin >> money;
+	if (money<7.5)
+		budget= "Not enough money.";
+	else if (money<10.5)
+		budget = "You can to Matinee show.";
+	else 
+		budget = "Can go to matine & evening show.";
+	cout << show << endl;
+	cout << budget << endl;
+	
+	return 0;
+}
