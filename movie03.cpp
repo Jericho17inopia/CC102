68ca251109bd244dcc4f91592dd15d25d89d22b5
Jericho Inopia <jerichoinopia7@gmail.com>
2024-11-28 12:43:17 +0800

    movie03.cpp

--- movie03.cpp
+++ movie03.cpp
@@ -1,42 +1,41 @@
-#include <iostream>
-
 using namespace std;
 
 int main() {
-	int age;
-	double money;
-	bool parent;
-	string show, budget;
-	cout << "Enter your age: ";
-	cin >> age;
-	if (age<13) {
-		cout << "with parent?";
-		cin >> parent;
-		if (parent) 
-			show="PG & G show.";
-		else
-			show="G show.";
-	}
-	else if (age<16) { 
-		cout << "with Parent?";
-		cin >> parent;
-		if (parent)
-			show="R, PG & G show";
-		else 
-			show = "PG & G show";
-	}
-	else
-		show="R, PG & G show.";
-	cout << "Enter money:";
-	cin >> money;
-	if (money<7.5)
-		budget= "Not enough money.";
-	else if (money<10.5)
-		budget = "You can to Matinee show.";
-	else 
-		budget = "Can go to matine & evening show.";
-	cout << show << endl;
-	cout << budget << endl;
-	
-	return 0;
+    int age, money;
+    char parent;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    cout << "Are you with a parent? (Y/N): ";
+    cin >> parent;
+
+    cout << "Enter the amount of money you have: ";
+    cin >> money;
+
+    if (age < 13) {
+        if (parent == 'Y') {
+            cout << "You can watch G or PG movies." << endl;
+        } else {
+            cout << "You can only watch G movies." << endl;
+        }
+    } else if (age >= 13 && age < 16) {
+        if (parent) {
+            cout << "You can watch G, PG, or R movies." << endl;
+        } else {
+            cout << "You can watch G, PG movies." << endl;
+        }
+    } else {
+        cout << "You can watch G, PG, or R movies." << endl;
+    }
+
+    if (money < 7.50) {        
+		cout << "You don't have enough money for a matinee show." << endl;
+    } else if (money < 10.50) {
+        cout << "You can only watch a matinee show." << endl;
+    } else {
+        cout << "You can watch a matinee or evening show." << endl;
+    }
+
+    return 0;
 }
