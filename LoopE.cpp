4db24e79e13f7a5cbc3dfadf2a63a3b825fe6ecc
Jericho Inopia <jerichoinopia7@gmail.com>
2024-11-30 11:23:39 +0800

    Add files via upload

---
+++ LoopE.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+
+using namespace std;
+
+int main () {
+	int rows = 8;
+	for (int i = 0; i <= rows; i++){
+		for (int j = 1; j <= rows; j++){
+			cout << " ";
+			}
+			for (int j = 1; j <= i; j++){
+				cout << j << " ";
+			}
+		cout << endl;
+	}
+	return 0;
+}
