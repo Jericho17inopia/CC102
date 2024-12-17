1673a8ed8472726b2989dfd78911c7fbd8b7b908
Jericho Inopia <jerichoinopia7@gmail.com>
2024-12-17 15:46:04 +0800

    Add files via upload

---
+++ Revised_Pattern01.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+        for (int i=0; i <7; i++){
+            for (int j=0; j < i; j++){
+                cout << "  ";
+            }
+       for (int j = i; j<7; j++){
+           if ((i % 2 == 0 && j % 2 == 1 || i % 2 == 1 && j % 2 == 1 )) {
+            cout << "@ ";   
+           } 
+            else {
+                cout << "# ";
+            }
+       }
+       cout << endl;
+   }
+    return 0;
+}
