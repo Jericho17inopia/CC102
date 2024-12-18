81059d7ef6d99d30ff122d853484823dc363c51a
Jericho Inopia <jerichoinopia7@gmail.com>
2024-12-18 13:15:08 +0800

    Add files via upload

---
+++ finalrevised01.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	for (int i=0; i<7; i++){
+		for(int j=0; j<i; j++){
+			cout << "  ";
+		}
+		for(int j=i; j<7; j++){
+		    if (j == 4 && i == 3 || j == 2 && i == 1 || j == 6 && i == 1 || j == 4 && i == 1){
+		        cout << "  ";
+		    }
+			else if((i % 2 == 0 && j % 2 == 1 || i % 2 == 1 && j % 2 == 1 )){
+				cout << "# ";
+			}
+			else if ((i % 2 == 0 && j % 2 == 0 || i % 2 == 1 && j % 2 == 1 )){
+				cout << "@ ";
+			}
+			else{
+				cout << "$ ";
+			}
+		}
+		cout << endl;
+	}
+	return 0;
+	
+}
